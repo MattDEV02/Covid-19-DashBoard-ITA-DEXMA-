9a62154170e8aca1f53637f406e784dc3b62ba17
MattDEV02 <matteolambertucci3@gmail.com>
2020-12-11 19:40:15 +0100

    axios request ok

--- backend/functions.php
+++ backend/functions.php
@@ -1,5 +1,21 @@
 <?php
 
+/* CORS POlICY */
+//-----------------------------------------------------------------------------------------------------
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+   header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+   header('Access-Control-Allow-Credentials: true');
+   header('Access-Control-Max-Age: 86400');
+}
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+      header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+   exit(0);
+}
+//-------------------------------------------------------------------------------------------------------------
+
 function getCSV($csv)
 {
    array_walk($csv, function (&$a) use ($csv) {
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+   componentDidMount() {
+      let data;
+      const url = 'http://127.0.0.1:8080/covid19/backend/';
+      axios.get(url)
+         .then(response => data = response.data)
+         .catch(error => console.error(error))
+         .then(() => console.log(data));
+   }
    render() {
       return (
          <div>
-            
          </div>
       );
    }
