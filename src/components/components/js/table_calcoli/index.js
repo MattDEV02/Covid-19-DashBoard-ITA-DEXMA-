778213edf8693d2a2d0f14e8c3a9a701bcaee448
MattDEV02 <matteolambertucci3@gmail.com>
2021-01-16 13:31:29 +0100

    finish.

--- src/components/components/js/table_calcoli/index.js
+++ src/components/components/js/table_calcoli/index.js
@@ -4,7 +4,7 @@ import {
 
 
 Array.prototype.sum = function () {
-   const intArr = this.map(elem => parseInt(elem));
+   const intArr = this.map(elem => parseInt(elem)); // All Array Element to Integer
    const sum = intArr.reduce((a, b) => ((a + b)));
    return sum;
 };
@@ -17,12 +17,12 @@ Array.prototype.last = function () {
 
 const randomColor = () => {
    const
-      hex = 0xffffff,
+      hex = 0XFFFFFF,
       random = Math.random();
    const x = Math.round((hex * random)).toString(16);
    const y = (6 - x.length);
    const z = ('000000').substring(0, y);
-   const code = String(z + x);
+   const code = new String(z + x);
    const result = ('#' + code);
    return result;
 };
@@ -37,12 +37,13 @@ let
 const arrayFetch = dati => {
    if (nuovi_positivi.length === 0) {
       dati.map(row => {
+         const
+            formattedData = formatData(row.data),
+            color = randomColor();
          nuovi_positivi.push(row.nuovi_positivi);
          ingressi_terapia_intensiva.push(row.ingressi_terapia_intensiva);
          variazioni_totale_positivi.push(row.variazione_totale_positivi);
-         const formattedData = formatData(row.data);
          date.push(formattedData);
-         const color = randomColor();
          colors.push(color);
       });
    }
@@ -108,14 +109,11 @@ const covid19Total = () => {
 };
 
 const covid19Media = (num, total) => {
-   const properties = ['nuovi_positivi', 'ingressi_terapia_intensiva', 'variazioni_totale_positivi'];
-   let
-      media = {},
-      i = 0;
-   for (const prop in total) { // For property in Object...
-      const result = (total[prop] / num).toFixed(2);
-      media[properties[i]] = result;
-      i++;
+   let media = {};
+   for (const prop in total) { // for property in Object...
+      const result = (total[prop] / num);
+      const result_fixed = result.toFixed(2)
+      media[prop] = result_fixed;
    }
    return media;
 };
@@ -134,7 +132,7 @@ const covid19Increment = () => {
    return increment;
 };
 
-const tooltipID = 'min-max';
+const tooltipID = 'tooltip-calcoli';
 
 const BgInfo = 'bg-info';
 
