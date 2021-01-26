9d5fa9614c3d4692bf1c2da182fd155e9b0abf62
MattDEV02 <matteolambertucci3@gmail.com>
2021-01-26 16:07:44 +0100

    finish.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,31 +5,34 @@ import './css/navbar/index.css';
 
 class Navbar extends React.Component {
    render() {
+      const
+         brand = nav.brand,
+         target = nav.target;
       return (
          <nav className='navbar navbar-expand-sm bg-dark navbar-dark navbar-custom justify-content-center'>
             <ul className='navbar-nav'>
                <li className={nav.item}>
-                  <a href={links[0]} className={nav.brand} target={nav.target}>
+                  <a href={links[0]} className={brand} target={target}>
                      Home
                   </a>
                </li>
                <li className={nav.item}>
-                  <a href={links[1]} className={nav.brand} target={nav.target}>
+                  <a href={links[1]} className={brand} target={target}>
                      Fonti
                   </a>
                </li>
                <li className={nav.item}>
-                  <a href={links[2]} className={nav.brand} target={nav.target}>
+                  <a href={links[2]} className={brand} target={target}>
                      Info
                   </a>
                </li>
                <li className={nav.item}>
-                  <a href={links[3]} className={nav.brand} target={nav.target}>
+                  <a href={links[3]} className={brand} target={target}>
                      Altro
                   </a>
                </li>
-               <li className={nav.item} target={nav.target}>
-                  <a href={links[4]} className={nav.brand}>
+               <li className={nav.item}>
+                  <a href={links[4]} className={brand} target={target}>
                      About
                   </a>
                </li>
